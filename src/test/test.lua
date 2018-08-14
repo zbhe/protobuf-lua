89028577080ac275e21714267641dbb1db15061d
zbhe <tony@debian-owncloud-opt>
2018-08-14 18:21:39 +0800

    add test

---
+++ src/test/test.lua
@@ -0,0 +1,13 @@
+lpb = require "lpb"
+lpb.add_proto_path("./src/test/")
+lpb.import_proto("test.proto")
+local msg = lpb.get_message("test.Person")
+local str = msg:encode({
+	name="Bjarne Stroustrup",
+	id = 1,
+	friends = {"Scott Meyers", "Stanley Lippman"},
+	email="bj@google.com"})
+local tbl =msg:decode(str)
+for k, v in pairs(tbl) do
+	print(k, v)
+end
